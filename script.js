dcb188a22e339864a5d41616018041976987efd6
moon-elxna <kreinelina@gmail.com>
2026-02-17 02:17:05 +0100

    fixed

--- script.js
+++ script.js
@@ -1,17 +1,90 @@
 //main
 const ctx = document.getElementById("canvas").getContext("2d");
+
 const counters = {
     flower: { current: 1, max: 5, img: null },
     stem: { current: 1, max: 5, img: null },
     decor: { current: 1 , max: 4, img: null }
 }
-//read_url();
 
 read_url();
+
 //functions
+function read_local_storage(){
+    function get_item(id){
+        const counter = counters[id];
+        let test = localStorage.getItem(id);
+        if(test != null){
+            test = parseInt(test);
+            if(test <= counter.max && test > 0){
+            counter.current = test; 
+        }
+        }  
+    }
+    get_item("flower"); 
+    get_item("stem"); 
+    get_item("decor");
+}
+
+function write_local_storage(){
+    function set_item(id){
+        const counter = counters[id];
+        localStorage.setItem(id, counter.current);
+    }
+    set_item("flower"); 
+    set_item("stem"); 
+    set_item("decor");
+}
+
+function share_url(){
+    //add parameters to link, "?parameter=value&parameter=value ..."
+    let url = "https://moon-elxna.github.io/build-ur-flower/?flower="+ counters.flower.current +"&stem=" + 
+        counters.stem.current + "&decor=" + counters.decor.current;
+    let text = "Check out my flower creation and build yours next!"
+    //write the string to the clipboard
+    navigator.clipboard.writeText(url);
+    alert("Link copied to clipboard!");
+}
+
+function download_img(){
+    //download link erstellen
+    const link = document.createElement("a"); 
+    // canvas zu png, toDataURL() = "Konvertiere Canvas zu PNG-Daten"
+    link.href = canvas.toDataURL("image/png");
+    //Dateiname setzen, 
+    link.download = "ur_flower.png"
+    //download starten
+    link.click();
+    alert("Downloaded as PNG!");
+}
+
+function read_url(){
+    //get params, params.get('') = grab the paramter from url, parseInt() = converts String to Integer
+    function get_params(id, params){
+        const counter = counters[id];
+        if (params.has(id)){
+            let val = parseInt(params.get(id)); 
+            //check if valid
+            if(val > counter.max || val <= 0){
+                counter.current = 1;
+            }
+            else{
+                counter.current = val
+            }     
+        }
+        else {
+            read_local_storage();
+        }
+    }
+    //grab parameters from the url, URLSearchParams = helper object for query parameters, windows = browser window, location = current page url, search = url after "?" 
+    const params = new URLSearchParams(window.location.search);
+    get_params("stem", params);
+    get_params("decor", params);
+    get_params("flower",params);
+    redraw_img();
+}
+
 async function redraw_img(){
-    //asynch function to redraw img
-    //function to loaf part of the img
     function load_img(id){
         const counter = counters[id];
         return new Promise(resolve => { //starts the Promise, waits until the resolved to finish function
@@ -23,11 +96,9 @@ async function redraw_img(){
             };
         });
     }
-    //function to draw part of the img
     function draw_img(img){
         ctx.drawImage(img,0,0);
     }
-   
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //await = waits until the fuction is finished, before excuting the next one
     await load_img("stem");
@@ -62,96 +133,4 @@ function change_img(id, flag){
         decrease_counter(counter);
     }
     redraw_img(id);
-    //document.getElementById(id).src = "img/" + id + "/" + counter.current + ".PNG";
-}
-
-function download_img(){
-   /* //neue canvas erstellen
-    const canvas = document.createElement("canvas");
-    canvas.width = 350;
-    canvas.height = 700;
-    // Zeichenwerzeug holen, ctx = context, 2d = 2d grafik
-    const ctx = canvas.getContext("2d");
-    //Bilder aus HTML holen
-    const flower_img = document.getElementById("flower");
-    const stem_img = document.getElementById("stem");
-    const decor_img = document.getElementById("decor");
-    //Bilder übereinader zeichen, 0,0 = oben-links Position
-    ctx.drawImage(stem_img,0,0);
-    ctx.drawImage(decor_img,0,0);
-    ctx.drawImage(flower_img,0,0);*/
-    //download link erstellen
-    const link = document.createElement("a"); 
-    // canvas zu png, toDataURL() = "Konvertiere Canvas zu PNG-Daten"
-    link.href = canvas.toDataURL("image/png");
-    //Dateiname setzen, 
-    link.download = "ur_flower.png"
-    //download starten
-    link.click();
-    alert("Downloaded as PNG!");
-}
-
-function share_url(){
-    //add parameters to link, "?parameter=value&parameter=value ..."
-    let url = "https://moon-elxna.github.io/build-ur-flower/?flower="+ counters.flower.current +"&stem=" + 
-        counters.stem.current + "&decor=" + counters.decor.current;
-    let text = "Check out my flower creation and build yours next!"
-    //write the string to the clipboard
-    navigator.clipboard.writeText(url);
-    alert("Link copied to clipboard!");
-}
-
-function read_url(){
-    //get params, params.get('') = grab the paramter from url, parseInt() = converts String to Integer
-    function get_params(id, params){
-        const counter = counters[id];
-        if (params.has(id)){
-            let val = parseInt(params.get(id)); 
-            //check if valid
-            if(val > counter.max || val <= 0){
-                counter.current = 1;
-            }
-            else{
-                counter.current = val
-            }     
-        }
-        else {
-            read_local_storage();
-        }
-    }
-    //grab parameters from the url, URLSearchParams = helper object for query parameters, windows = browser window, location = current page url, search = url after "?" 
-    const params = new URLSearchParams(window.location.search);
-    get_params("stem", params);
-    get_params("decor", params);
-    get_params("flower",params);
-    redraw_img();
-    
-}
-
-function read_local_storage(){
-
-    function get_item(id){
-        const counter = counters[id];
-        let test = localStorage.getItem(id);
-        if(test != null){
-            test = parseInt(test);
-            if(test <= counter.max && test > 0){
-            counter.current = test; 
-        }
-        }
-        
-    }
-    get_item("flower"); 
-    get_item("stem"); 
-    get_item("decor");
-}
-
-function write_local_storage(){
-    function set_item(id){
-        const counter = counters[id];
-        localStorage.setItem(id, counter.current);
-    }
-    set_item("flower"); 
-    set_item("stem"); 
-    set_item("decor");
 }
