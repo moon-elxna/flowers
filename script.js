332d70d782c3f27ccebcaf29ea9380825dc8a404
moon-elxna <kreinelina@gmail.com>
2026-02-21 00:40:57 +0100

    sorted messy code

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 //---main---
-const counters = {
-    flower: { current: 1, max: 5},
-    stem: { current: 1, max: 5},
-    decor: { current: 1 , max: 4}
-}
 const parts = ["stem", "decor","flower"]
+const counters = {
+    stem: { current: 1, max: 5},
+    decor: { current: 1, max: 5},
+    flower: { current: 1 , max: 4}
+}
 read_url();
 
 //---addEventListeners---
-//left btns
-arr_btn("l", false);
-//right btns
-arr_btn("r", true);
-//tool btn
-document.getElementById("btn_dwn").addEventListener("click", function(){download_img()});
-document.getElementById("btn_share").addEventListener("click", function(){share_url()})
+arr_btn("l", false); //left btns
+arr_btn("r", true); //right btns
+document.getElementById("btn_dwn").addEventListener("click", function(){download_img()}); //tool btns
+document.getElementById("btn_share").addEventListener("click", function(){share_url()}) //tool btns
+
 //---functions---
 function arr_btn(dir, flag){
     for(let i = 0; i<parts.length; i++){
@@ -23,6 +21,10 @@ function arr_btn(dir, flag){
 }
 
 function read_local_storage(){
+    for(let i = 0; i < parts.length; i++){
+        get_item(parts[i])
+    }
+
     function get_item(id){
         const counter = counters[id];
         let test = localStorage.getItem(id);
@@ -33,68 +35,60 @@ function read_local_storage(){
         }
         }  
     }
-    get_item("flower"); 
-    get_item("stem"); 
-    get_item("decor");
 }
 
 function write_local_storage(){
+    for(let i = 0; i < parts.length; i++){
+        set_item(parts[i])
+    }
+
     function set_item(id){
         const counter = counters[id];
         localStorage.setItem(id, counter.current);
     }
-    for(let i = 0; i < parts.length; i++){
-        set_item(parts[i])
-    }
 }
 
 function share_url(){
-    //add parameters to link, "?parameter=value&parameter=value ..."
-    let url = "https://moon-elxna.github.io/build-ur-flower/?flower="+ counters.flower.current +"&stem=" + 
-        counters.stem.current + "&decor=" + counters.decor.current;
-    let text = "Check out my flower creation and build yours next!"
-    //write the string to the clipboard
-    navigator.clipboard.writeText(url);
+    let url = "https://moon-elxna.github.io/build-ur-flower/?flower="
+        + counters.flower.current +"&stem=" + counters.stem.current + "&decor=" 
+        + counters.decor.current; //add parameters to link, "?parameter=value&parameter=value ..."
+    navigator.clipboard.writeText("Check out my flower creation and build yours next!\n" + url); //write string to clipboard
     alert("Link to your flower was copied to your clipboard!");
 }
 
 function download_img(){
-    //neue canvas erstellen
-    const canvas = document.createElement("canvas");
-    canvas.width = 350;
-    canvas.height = 700;
-    // Zeichenwerzeug holen, ctx = context, 2d = 2d grafik
-    const ctx = canvas.getContext("2d");
-    //add solid bg
-    ctx.fillStyle = "#fffafb";
+    const canvas = document.createElement("canvas"); //create canvas
+    canvas.width = 350; canvas.height = 700;
+    const ctx = canvas.getContext("2d"); // get drawing tools in 2d
+
+    ctx.fillStyle = "#fffafb"; //add solid bg
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    //
+
+    const img_name = document.getElementById("name"); //get img from html
+    ctx.drawImage(img_name, 0, 0); // draw img in postion 0,0 (top-left),btw order like in code
     for(let i = 0; i < parts.length; i++){
         const img = document.getElementById(parts[i]);
         ctx.drawImage(img, 0, 0);
     }
-    //Bilder aus HTML holen
-    const img_name = document.getElementById("name");
-    //Bilder übereinader zeichen, 0,0 = oben-links Position
-    ctx.drawImage(img_name, 0, 0);
-    //download link erstellen
-    const link = document.createElement("a"); 
-    // canvas zu png, toDataURL() = "Konvertiere Canvas zu PNG-Daten"
-    link.href = canvas.toDataURL("image/png");
-    //Dateiname setzen, 
-    link.download = "ur_flower.png"
-    //download starten
-    link.click();
-    //alert("Downloaded as PNG!"); 
+    
+    const link = document.createElement("a"); //create dwn link
+    link.href = canvas.toDataURL("image/png"); // canvas to png w toDataURL() 
+    link.download = "ur_flower.png" //filename
+    link.click(); //start dwn
 }
 
 function read_url(){
-   //get params, params.get('') = grab the paramter from url, parseInt() = converts String to Integer
+    const params = new URLSearchParams(window.location.search); //grab parameters from the url, URLSearchParams = helper object for query parameters, windows = browser window, location = current page url, search = url after "?" 
+    for(let i = 0; i < parts.length; i++){
+        get_params(parts[i],params);
+    }
+    write_local_storage();
+
     function get_params(id, params){
         const counter = counters[id];
         flag = false;
         if (params.has(id)){
-            let val = parseInt(params.get(id)); 
+            let val = parseInt(params.get(id)); //params.get('') = grab the paramter from url, parseInt() = converts String to Integer
             //check if valid
             if(val > counter.max || val <= 0){
                 counter.current = 1;
@@ -109,15 +103,19 @@ function read_url(){
             read_local_storage();
         }
     }
-    //grab parameters from the url, URLSearchParams = helper object for query parameters, windows = browser window, location = current page url, search = url after "?" 
-    const params = new URLSearchParams(window.location.search);
-    for(let i = 0; i < parts.length; i++){
-         get_params(parts[i],params);
-    }
-    write_local_storage();
 }
 
 function change_img(id, flag){
+    const counter = counters[id];
+    if(flag == true){ 
+        increase_counter(counter);
+    } 
+    else if(flag == false){
+        decrease_counter(counter);
+    }   
+    document.getElementById(id).src = "assets/" + id + "/" + counter.current + ".PNG";
+    write_local_storage();
+
     function decrease_counter(counter){
         if(counter.current == 1){
                 counter.current = counter.max;
@@ -132,13 +130,4 @@ function change_img(id, flag){
                 counter.current = counter.current + 1;
             }
     }
-    const counter = counters[id];
-    if(flag == true){ 
-        increase_counter(counter);
-    } 
-    else if(flag == false){
-        decrease_counter(counter);
-    }   
-    document.getElementById(id).src = "assets/" + id + "/" + counter.current + ".PNG";
-    write_local_storage();
 }
